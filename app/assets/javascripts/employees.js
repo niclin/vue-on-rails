fe99f8dacf995aa9be0e41b33b1e2b4dc1776843
niclin <nic.lin@abagile.com>
2017-01-17 15:38:21 +0800

    Updating an employee

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -2,6 +2,36 @@ Vue.component('employee-row', {
   template: '#employee-row',
   props: {
     employee: Object
+  },
+  data: function () {
+    return {
+      editMode: false,
+      errors: {}
+    }
+  },
+  methods: {
+    toggleManagerStatus: function () {
+      this.employee.manager = !this.employee.manager
+      this.updateEmployee()
+    },
+    updateEmployee: function () {
+      var that = this;
+      $.ajax({
+        method: 'PUT',
+        data: {
+          employee: that.employee,
+        },
+        url: '/employees/' + that.employee.id + '.json',
+        success: function(res) {
+          that.errors = {}
+          that.employee = res
+          that.editMode = false
+        },
+        error: function(res) {
+          that.errors = res.responseJSON.errors
+        }
+      })
+    }
   }
 })
 
